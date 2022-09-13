e364cfb1ef63961b1c44ebd293c6c0d9e759bfe9
Little Clover <littleclover909@gmail.com>
2022-09-13 11:21:53 +0800

    doc: update

--- doc/html/search/all_1.js
+++ doc/html/search/all_1.js
@@ -5,7 +5,8 @@ var searchData=
   ['http_5fheader_5flen_2',['HTTP_HEADER_LEN',['../http-handler_8h.html#aadf9bf10be789e7bd38610742e76be4c',1,'http-handler.h']]],
   ['http_5frequest_5fmethod_5flen_3',['HTTP_REQUEST_METHOD_LEN',['../http-handler_8h.html#a49180fdd018be2adfe4df3a59482ab0c',1,'http-handler.h']]],
   ['http_5frequest_5fpath_5flen_4',['HTTP_REQUEST_PATH_LEN',['../http-handler_8h.html#a7d0073152dc4351d8b645031b83093d0',1,'http-handler.h']]],
-  ['http_5frequest_5fversion_5flen_5',['HTTP_REQUEST_VERSION_LEN',['../http-handler_8h.html#a158d838bff67042768ab941e496265c5',1,'http-handler.h']]],
-  ['httpmethod_6',['httpMethod',['../http-handler_8h.html#a2e2c772c713591600f7bee7d9bf3f3fe',1,'http-handler.h']]],
-  ['httpstatus_7',['httpStatus',['../http-handler_8h.html#abd1605a1ce8475ed58ef3759e0547776',1,'http-handler.h']]]
+  ['http_5frequest_5fstatus_5fline_5flen_5',['HTTP_REQUEST_STATUS_LINE_LEN',['../http-handler_8h.html#a3a45a28f0d29979383f09278aad2ee57',1,'http-handler.h']]],
+  ['http_5frequest_5fversion_5flen_6',['HTTP_REQUEST_VERSION_LEN',['../http-handler_8h.html#a158d838bff67042768ab941e496265c5',1,'http-handler.h']]],
+  ['httpmethod_7',['httpMethod',['../http-handler_8h.html#a2e2c772c713591600f7bee7d9bf3f3fe',1,'http-handler.h']]],
+  ['httpstatus_8',['httpStatus',['../http-handler_8h.html#abd1605a1ce8475ed58ef3759e0547776',1,'http-handler.h']]]
 ];
--- doc/html/search/defines_1.js
+++ doc/html/search/defines_1.js
@@ -3,5 +3,6 @@ var searchData=
   ['http_5fheader_5flen_0',['HTTP_HEADER_LEN',['../http-handler_8h.html#aadf9bf10be789e7bd38610742e76be4c',1,'http-handler.h']]],
   ['http_5frequest_5fmethod_5flen_1',['HTTP_REQUEST_METHOD_LEN',['../http-handler_8h.html#a49180fdd018be2adfe4df3a59482ab0c',1,'http-handler.h']]],
   ['http_5frequest_5fpath_5flen_2',['HTTP_REQUEST_PATH_LEN',['../http-handler_8h.html#a7d0073152dc4351d8b645031b83093d0',1,'http-handler.h']]],
-  ['http_5frequest_5fversion_5flen_3',['HTTP_REQUEST_VERSION_LEN',['../http-handler_8h.html#a158d838bff67042768ab941e496265c5',1,'http-handler.h']]]
+  ['http_5frequest_5fstatus_5fline_5flen_3',['HTTP_REQUEST_STATUS_LINE_LEN',['../http-handler_8h.html#a3a45a28f0d29979383f09278aad2ee57',1,'http-handler.h']]],
+  ['http_5frequest_5fversion_5flen_4',['HTTP_REQUEST_VERSION_LEN',['../http-handler_8h.html#a158d838bff67042768ab941e496265c5',1,'http-handler.h']]]
 ];
