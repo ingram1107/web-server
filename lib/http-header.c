9341b1d7d771a8687cdebea5ac796ea0d224b153
Little Clover <littleclover909@gmail.com>
2022-09-14 17:20:17 +0800

    fix: program doesn't exit immediately when not enough memory

--- lib/http-header.c
+++ lib/http-header.c
@@ -1,9 +1,17 @@
 #include "http-header.h"
 
+#include <asm-generic/errno-base.h>
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 HTTPHeader* httpHeaderInit(void) {
   HTTPHeader* newHTTPHeader = malloc(sizeof(struct HTTPHeader));
+  if (!newHTTPHeader) {
+    errno = ENOMEM;
+    perror("http-header");
+    exit(EXIT_FAILURE);
+  }
   return newHTTPHeader;
 }
 
