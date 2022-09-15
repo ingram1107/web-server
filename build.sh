844bfa60017c18be29bc2032e6e76de9a0e16a4f
Little Clover <littleclover909@gmail.com>
2022-09-15 22:14:20 +0800

    add AStyle as the formatting tool

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ cmake -S . -B $build_directory $1 && cd $build_directory && make doc && make && 
 if [ ! -f compile_commands.json ]; then
   ln -s $build_directory/compile_commands.json compile_commands.json
 fi
+
+astyle --project=web-server/.astylerc src/*.c lib/*.h lib/*.c
