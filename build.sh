b55edbab88868395045d2818927147a7be579819
Little Clover <littleclover909@gmail.com>
2022-09-15 16:15:23 +0800

    incorporate static sanitisers into CMake build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 source ./config.txt
-cmake -S . -B $build_directory && cd $build_directory && make doc && make && cd ..
+cmake -S . -B $build_directory $1 && cd $build_directory && make doc && make && cd ..
 
 if [ ! -f compile_commands.json ]; then
   ln -s $build_directory/compile_commands.json compile_commands.json
